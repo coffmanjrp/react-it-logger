891c45cc0b1073bf31a30993166e5937561b80b6
coffmanjrp <coffmanjrp@gmail.com>
2021-02-16 19:07:32 -0800

    remove useState hook from TechListModal component

--- src/components/techs/TechListModal.jsx
+++ src/components/techs/TechListModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { TechItem } from './';
 import { getTechs } from '../../actions/techActions';
